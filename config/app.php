7d18b073ec71fda2b7054d80833e8e654223fd20
soni-code <lx@af3w.com>
2017-06-10 15:01:39 +0800

    Finish user signup

--- config/app.php
+++ config/app.php
@@ -36,10 +36,10 @@ return [
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
-    |
+    | UTC
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'PRC',
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
